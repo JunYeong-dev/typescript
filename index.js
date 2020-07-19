1edcf8d8be77e8d6b6f71a22c2f349c8065ae01b
Jun-cloud <kjy8827@gmail.com>
2020-07-19 12:49:05 +0900

    Create typescript function

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("Type Script");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Judy", age = 18, gender = "female";
+const sayHello = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age},ya are a ${gender}`);
+};
+sayHello(name, age, gender);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("Type Script");
+const name = "Judy",
+    age = 18,
+    gender = "female";
+/*
+name, age, gender? <- 매개변수에 ?를 붙여 매개변수를 옵션화할 수 있음
+sayHello(name, age); <- 실행하게 되면 Hello Judy, you are 18,ya are a undefined 가 출력됨
+*/
+const sayHello = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+}
+
+sayHello(name, age, gender);
+
+export{};
