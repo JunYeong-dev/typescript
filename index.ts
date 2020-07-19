c1905d14468f5f849b16020856a21576c03262a3
Jun-cloud <kjy8827@gmail.com>
2020-07-19 13:00:00 +0900

    Function type - parameter, return

--- index.ts
+++ index.ts
@@ -4,8 +4,10 @@ const name = "Judy",
 /*
 name, age, gender? <- 매개변수에 ?를 붙여 매개변수를 옵션화할 수 있음
 sayHello(name, age); <- 실행하게 되면 Hello Judy, you are 18,ya are a undefined 가 출력됨
+
+매개변수의 type와 리턴 type을 정해줄 수 있다
 */
-const sayHello = (name, age, gender) => {
+const sayHello = (name: string, age: number, gender: string): void => {
     console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
 }
 
