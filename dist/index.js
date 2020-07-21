841067ae188eeccf51bd706284f6f306b8eef3d6
Jun-cloud <kjy8827@gmail.com>
2020-07-21 21:12:39 +0900

    Create interface

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const name = "Judy", age = 18, gender = "female";
+const person = {
+    name: "Judy",
+    age: 18,
+    gender: "female"
+};
 /*
 name, age, gender? <- 매개변수에 ?를 붙여 매개변수를 옵션화할 수 있음
 sayHello(name, age); <- 실행하게 되면 Hello Judy, you are 18,ya are a undefined 가 출력됨
 
 매개변수의 type와 리턴 type을 정해줄 수 있다
 */
-const sayHello = (name, age, gender) => {
-    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+const sayHello = (person) => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 };
-sayHello(name, age, gender);
+console.log(sayHello(person));
 //# sourceMappingURL=index.js.map
